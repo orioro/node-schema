11c04734e0499e08f2acef86c7258e8c8671d8d4
Simon Fan <simon@orioro.design>
2021-02-22 15:24:29 -0300

    refactor: remove dead code

--- src/expressions.ts
+++ src/expressions.ts
@@ -2,8 +2,6 @@ import { ExpressionInterpreter, interpreter } from '@orioro/expression'
 
 import { getType as _getType } from '@orioro/validate-type'
 
-const defaultGetType = (value) => undefined // eslint-disable-line @typescript-eslint/no-unused-vars
-
 export type GetTypeInterface = (value: any) => string | void
 
 export const schemaTypeExpression = (
