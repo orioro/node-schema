2dca549b0205cb91f12db2654c3d8a6143364eb5
Simon Fan <simon@orioro.design>
2021-04-27 17:16:02 -0300

    feat: update @orioro/expression and @orioro/validate dependencies

--- src/schemaEnv.spec.ts
+++ src/schemaEnv.spec.ts
@@ -1,7 +1,7 @@
 import { schemaEnv } from './schemaEnv'
 
 describe('schemaEnv - basic', () => {
-  const { resolveSchema, resolveValue, validate } = schemaEnv()
+  const { resolveSchema, resolveValue, validateSync } = schemaEnv()
 
   const stringSchema = {
     type: 'string',
@@ -50,23 +50,23 @@ describe('schemaEnv - basic', () => {
     ).toEqual(['123', '12345', '1234567890', 8])
   })
 
-  test('validate(resolvedSchema, resolvedValue)', () => {
+  test('validateSync(resolvedSchema, resolvedValue)', () => {
     const v1 = undefined
     const rs1 = resolveSchema(schema, v1) // resolvedSchema
     const rv1 = resolveValue(rs1, v1) // resolvedValue
-    expect(validate(rs1, rv1)).toEqual(null)
+    expect(validateSync(rs1, rv1)).toEqual(null)
 
     const v2 = '123'
     const rs2 = resolveSchema(schema, v2)
     const rv2 = resolveValue(rs2, v2)
-    expect(validate(rs2, rv2)).toMatchObject([
+    expect(validateSync(rs2, rv2)).toMatchObject([
       { code: 'STRING_MIN_LENGTH_ERROR', path: '' },
     ])
 
     const v3 = ['123', '12345', '12345678901', 8]
     const rs3 = resolveSchema(schema, v3)
     const rv3 = resolveValue(rs3, v3)
-    expect(validate(rs3, rv3)).toMatchObject([
+    expect(validateSync(rs3, rv3)).toMatchObject([
       { code: 'STRING_MIN_LENGTH_ERROR', path: '0' },
       { code: 'STRING_MAX_LENGTH_ERROR', path: '2' },
       { code: 'TYPE_ERROR', path: '3' },
